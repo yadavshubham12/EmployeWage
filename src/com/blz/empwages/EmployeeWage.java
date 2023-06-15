a3340ca4c7fb51670b851a7ad48c47b010b81c4b
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-06-15 23:18:28 +0530

    ADDED: Employee daily salary

--- src/com/blz/empwages/EmployeeWage.java
+++ src/com/blz/empwages/EmployeeWage.java
@@ -6,12 +6,17 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation programme");
         int isPresent=1;
+        int wagePerHr=20;
+        int fullDayHr=8;
+        double salary;
         Random random = new Random();
         int randomCheck = random.nextInt(2);
         System.out.println(randomCheck);
 
         if (randomCheck == isPresent){
             System.out.println("Employee is present");
+          salary = (fullDayHr * wagePerHr);
+            System.out.println("DailyWage of employee is :" +salary);
         }
         else {
             System.out.println("Employee is abscent");
