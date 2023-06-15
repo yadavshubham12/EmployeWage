471abb8085714267a4275b23ea5fec9e2e7bd92c
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-06-15 18:51:09 +0530

    Added: HelloWorld_Program

---
+++ src/com/blz/empwages/EmployeeWage.java
@@ -0,0 +1,8 @@
+package com.blz.empwages;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation programme");
+
+    }
+}
