6a998ff2e1aa45a8b7708b785de71f3b0b01c179
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-06-15 19:02:41 +0530

    ADDED: UC1_Employee_is_present

--- src/com/blz/empwages/EmployeeWage.java
+++ src/com/blz/empwages/EmployeeWage.java
@@ -1,8 +1,20 @@
 package com.blz.empwages;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation programme");
+        int isPresent=1;
+        Random random = new Random();
+        int randomCheck = random.nextInt(2);
+        System.out.println(randomCheck);
 
+        if (randomCheck == isPresent){
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is abscent");
+        }
     }
 }
