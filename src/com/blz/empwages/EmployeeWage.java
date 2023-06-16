49c9b34379259730410789e9e4cb5ab9f842ecb2
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-06-17 00:13:50 +0530

    ADDED: Employee monthly salary

--- src/com/blz/empwages/EmployeeWage.java
+++ src/com/blz/empwages/EmployeeWage.java
@@ -5,33 +5,35 @@ import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation programme");
-        int isFullTime=1;
-        int isPartTIme=2;
-        int wagePerHr=20;
-        int fullDayHr=8;
-        int halfDayHr=4;
-        double salary = 0;
+        int isFullTime = 1;
+        int isPartTIme = 2;
+        int wagePerHr = 20;
+        int fullDayHr = 8;
+        int halfDayHr = 4;
+        int workingDays = 20;
+        double dailySalary = 0;
+        double monthlyWage = 0;
+
         Random random = new Random();
         int randomCheck = random.nextInt(3);
         System.out.println(randomCheck);
 
-        switch (randomCheck){
-            case 1: {
-                System.out.println("Employee is fullDay present: ");
-                salary = fullDayHr * wagePerHr;
-            }
-            break;
-            case 2: {
-                System.out.println("Employee is HalfDay Present: ");
-                salary = halfDayHr * wagePerHr;
-            }
-            break;
-            case 3: {
-                System.out.println("Employee is Absent");
-            }
-            break;
+        if (randomCheck == isFullTime){
+            System.out.println(" Is fullDayPresent : ");
+            dailySalary = wagePerHr * fullDayHr;
+            System.out.println("DailyWage :" + dailySalary);
+            monthlyWage =(dailySalary * workingDays);
+            System.out.println("Empoloyee Monthly Salary is : " +monthlyWage );
         }
-        System.out.println("Salary of employee is: " +salary);
-        System.out.println("Employee Salary: " +salary);
+        else if (randomCheck == isPartTIme) {
+            System.out.println("is HalfDayPresent : ");
+            dailySalary = wagePerHr * halfDayHr;
+            System.out.println("HalfDayPresent salary : " +dailySalary);
+            monthlyWage = dailySalary * workingDays;
+            System.out.println("Employee monthly salary is : " +monthlyWage);
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }
